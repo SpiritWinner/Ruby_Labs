234957c1147eca6175683bb8a7a0cfaf6e3aa97d
SpiritWinner <spirit.no.win@gmail.com>
2021-12-21 15:36:12 +0300

    add first task, done

--- lab3/ferst_task/main.rb
+++ lab3/ferst_task/main.rb
@@ -5,17 +5,23 @@ def index
   File.foreach(STUDENT_LIST_PATH) { |student| puts student }
 end
 
-def where(name)
+def find(id)
   File.foreach(STUDENT_LIST_PATH).with_index do |student, index|
-    @student_id = index if student.include?(name)
+    puts student if id == index
   end
-  @student_id
 end
 
-def update(id, name)
+def where(pattern)
+  File.foreach(STUDENT_LIST_PATH).with_index do |student, index|
+    @student_id = index if student.include?(pattern)
+  end
+  puts(@student_id)
+end
+
+def update(id, text)
   file = File.open(BUFFER, 'w')
   File.foreach(STUDENT_LIST_PATH).with_index do |student, index|
-    file.puts(id == index ? name : student)
+    file.puts(id == index ? text : student)
   end
 
   file.close
@@ -26,12 +32,16 @@ end
 def delete(id)
   file = File.open(BUFFER, 'w')
   File.foreach(STUDENT_LIST_PATH).with_index do |student, index|
-    if id != index
-      file.puts(student)
-    end
+    file.puts(student) if id != index
   end
 
   file.close
   File.write(STUDENT_LIST_PATH, File.read(BUFFER))
   File.delete(BUFFER) if File.exist?(BUFFER)
 end
+
+index
+find(2)
+where('Ai')
+update(0, 'Akuloy Nishail')
+delete(1)
