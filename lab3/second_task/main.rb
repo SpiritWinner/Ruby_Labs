ce6fe8166b8611f723bea6c47bee0b5c2fd9e7df
SpiritWinner <spirit.no.win@gmail.com>
2021-12-22 02:13:02 +0300

    added second task

--- lab3/second_task/main.rb
+++ lab3/second_task/main.rb
@@ -1,27 +1,46 @@
 STUDENT_LIST_PATH = './student.txt'
-RESULT = './results.txt
-'
+RESULT = './results.txt'
 
-def get_data
-  puts "Please enter age of student or \"-1\" for exit"
-  gets.to_i
-end
-
-def read
+def read_file
   students = []
-  File.foreach(STUDENT_LIST_PATH) { |line| students.push(line.chomp) }
+  file = File.open(STUDENT_LIST_PATH)
+  file.readlines.each do |student|
+    temp = student.split(' ')
+    students << [temp[0], temp[1], temp[2].chomp.to_i]
+  end
+  file.close
+  students
 end
 
-def write_fail
-for stud in students
-  File.write(RESULT, "#{stud}\n", mode: "a") if stud.include?("#{age}")
-  students.delete(stud) if stud.include?("#{age}")
+def write_file(content)
+  file = File.open(RESULT, 'w')
+  content.each do |student|
+    file.write "Student: #{student[0]} #{student[1]} \tage: #{student[2]}\n"
+  end
+  file.close
 end
 
+def main
+  students = []
+  stedent_list = read_file
+ 
+  loop do
+    puts 'Please enter age of student or "-1" for exit'
+    age = gets.to_i
+    break if age == -1
 
-def write_in_consol
-  File.foreach(STUDENT_LIST_PATH) { |student| puts student }
+    flag = false
+    stedent_list.each do |student|
+      if student[2] == age
+        students << student
+        flag = true
+      end
+    end
+    puts "Don't have student with current age" unless flag
+  end
+
+  write_file(students)
+  students
 end
 
-read
-write_in_consol
+main
