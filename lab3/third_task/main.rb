2082c75a4215f521c15220a545182b61ffe6ccba
SpiritWinner <spirit.no.win@gmail.com>
2021-12-22 02:33:24 +0300

    ieie

--- lab3/third_task/main.rb
+++ lab3/third_task/main.rb
@@ -17,3 +17,73 @@ def balance(total)
   print "Balance: #{total}\n"
 end
 
+def check_balance
+  print "Money deposit print \"D\"\nWithdraw money print \"W\"\nCheck balance print \"B\"\nFor exit print \"Q\"\n"
+  gets.chomp.to_s.downcase
+end
+
+def deposit (total)
+  total = total.to_i
+
+  def dep_money(total)
+    balance(total)
+    print "Deposit amount: "
+    sum = gets.to_i
+    if sum > 0
+      sum
+    else
+      print "Enter the correct number\n"
+      dep_money(total)
+    end
+  end
+
+  sum = dep_money(total)
+  print "Deposit: #{sum}, ur balance #{total + sum}\n"
+  (total + sum)
+end
+
+def withdraw (total)
+  total = total.to_i
+
+  def with_money(total)
+    balance(total)
+    print "Withdraw amount"
+    sum = gets.to_i
+    if sum > total
+      print "Ur trying to get more than ur balance\n"
+      with_money(total)
+    else
+      sum
+    end
+  end
+
+  sum = with_money(total)
+  print "Withdraw: #{sum}, ur balance #{total - sum}\n"
+  (total - sum)
+end
+
+def error
+  print "Wrong button, please try again\n"
+end
+
+def main
+  button = "b"
+  money = read_file
+  while button != "q"
+    case button
+    when "b"
+      balance(money)
+    when "d"
+      money = deposit(money)
+      when "w"
+        money = withdraw(money)
+        else
+          error
+    end
+    button = check_balance
+  end
+  write_file(money)
+  money
+end
+
+main
