984fe7db3d9cb173a84d6e6123e7ebf5ee086afc
SpiritWinner <spirit.no.win@gmail.com>
2021-12-21 15:19:36 +0300

    add delete

--- lab3/ferst_task/main.rb
+++ lab3/ferst_task/main.rb
@@ -1,26 +1,37 @@
-ACTORS_LIST_PATH = 'artists.txt'
-BUFFER = 'buffer.txt'
+STUDENT_LIST_PATH = './student.txt'
+BUFFER = './buffer.txt'
 
 def index
-  File.foreach(ACTORS_LIST_PATH) { |actor| puts actor }
+  File.foreach(STUDENT_LIST_PATH) { |student| puts student }
 end
 
 def where(name)
-  File.foreach(ACTORS_LIST_PATH).with_index do |actor, index|
-    @actor_id = index if actor.include?(name)
+  File.foreach(STUDENT_LIST_PATH).with_index do |student, index|
+    @student_id = index if student.include?(name)
   end
-
-  @actor_id
+  @student_id
 end
 
 def update(id, name)
   file = File.open(BUFFER, 'w')
-  File.foreach(ACTORS_LIST_PATH).with_index do |actor, index|
-    file.puts(id == index ? name : actor)
+  File.foreach(STUDENT_LIST_PATH).with_index do |student, index|
+    file.puts(id == index ? name : student)
   end
 
   file.close
-  File.write(ACTORS_LIST_PATH, File.read(BUFFER))
+  File.write(STUDENT_LIST_PATH, File.read(BUFFER))
+  File.delete(BUFFER) if File.exist?(BUFFER)
+end
 
+def delete(id)
+  file = File.open(BUFFER, 'w')
+  File.foreach(STUDENT_LIST_PATH).with_index do |student, index|
+    if id != index
+      file.puts(student)
+    end
+  end
+
+  file.close
+  File.write(STUDENT_LIST_PATH, File.read(BUFFER))
   File.delete(BUFFER) if File.exist?(BUFFER)
 end
